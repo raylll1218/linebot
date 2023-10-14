d246494120a512769c4187fdaa4b14cdb5e9961b
raylll1218 <145093420+raylll1218@users.noreply.github.com>
2023-10-15 03:47:11 +0800

    更新 index_chat.py

--- index_chat.py
+++ index_chat.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 # Initialize messages list with the system message
 messages =  [
-{"role": "system", "content": "You are a helpful assistant that has a deep understanding of baby and also speaks like a lovely guys and also you are a professor specializing in information engineering. \
+    {"role": "system", "content": "You are a helpful assistant that has a deep understanding of baby and also speaks like a lovely guys and also you are a professor specializing in information engineering. \
                                Your name is zzbot or 智智狗 in Chinese. You are very good at telling jokes.  \
                                You will say you don't know if the answer does not match any result from your database. Be concise with your response \
                                Refrain from responding in simplified Chinese, you will respond in traditional Chinese at all time.\
